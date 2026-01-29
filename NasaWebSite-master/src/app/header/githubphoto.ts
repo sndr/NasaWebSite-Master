93373e501dea20cd68be5f7706da644d3299a1f8
sndr <alexsanderbeny3707@gmail.com>
2026-01-29 15:57:59 +0100

    added the press key as enter

--- NasaWebSite-master/src/app/header/githubphoto.ts
+++ NasaWebSite-master/src/app/header/githubphoto.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
- selector: 'github-photo',
+  selector: 'github-photo',
   template: `<a href="https://github.com/sndr" class="github-link" aria-label="GitHub"><img src="/assets/header/github-mark-white.png" alt="GitHub" width="24" height="24"></a>`,
 })
 export class Githubphoto { }
